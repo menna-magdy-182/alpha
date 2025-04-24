6bbf705762d3baf0672ccac18d30006df217e0fa
menna magdy <menna.magdy@nfq.com>
2025-04-24 20:43:06 +0200

    adjust statusbar color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
+import Colors from "@/theme/colors";
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack initialRouteName="(tabs)" screenOptions={{ header: () => null }}>
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    <>
+      <Stack initialRouteName="(tabs)" screenOptions={{ header: () => null }}>
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+      <StatusBar backgroundColor={Colors.background} barStyle="light-content" />
+    </>
   );
 }
